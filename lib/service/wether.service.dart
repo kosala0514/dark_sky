a4ea4a7fbe30e78a275656c26faf54e1b859b5ea
CharunuKJ <charunikosala9723@gmail.com>
2022-08-10 16:51:37 +0530

    second commit

--- lib/service/wether.service.dart
+++ lib/service/wether.service.dart
@@ -6,7 +6,7 @@ import 'package:weather_flutter_rest_api/models/wether.model.dart';
 class WeatherService {
 // https:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
   Future<WeatherModel> getWeather(String lat, String lon) async {
-    const apiKey = '8f1582d52d888f1b8b128f25b433eac6';
+    const apiKey = 'API_Key';
 
     final uri = Uri.parse(
         'http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey');
